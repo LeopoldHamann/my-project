eddb81939fd5383dd611f871347bb6b3ac4c5f08
LeopoldHamann <leopold@a-hamann.de>
2025-07-09 21:08:28 +0200

    bug fixes
    added the main code

---
+++ src/basic.js
@@ -0,0 +1,10 @@
+console.log('This is a basic module.');
+AudioDecoder.prototype.decode = function() {
+    console.log('Decoding audio...');
+    }
+
+
+
+    GamepadButton.prototype.press = function() {
+        console.log('Button pressed!');
+    
